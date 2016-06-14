3e8ea5998f5e68ea0207cb6d1547bd5403f4b281
Jonamir Ian Ventura <jonianven@gmail.com>
2016-06-14 06:31:42 +0000

    Used downto & upto instead of ranges

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -6,46 +6,46 @@ class Piece < ActiveRecord::Base
     return true if self.game.is_occupied?(dest_x, dest_y) && self.player == self.game.pieces.where(position_x:dest_x, position_y: dest_y).first.player
     if self.position_y == dest_y #Horizontal
       if self.position_x < dest_x
-        (position_x + 1.. dest_x - 1).each do |x|
+        (position_x + 1).upto(dest_x - 1) do |x|
           return true if self.game.is_occupied?(x, dest_y)
         end 
       else
-        (position_x - 1.. dest_x + 1).each do |x|
+        (position_x - 1).downto(dest_x + 1) do |x|
           return true if self.game.is_occupied?(x, dest_y)
         end 
       end 
     elsif self.position_x == dest_x #Vertical
       if self.position_y < dest_y
-        (position_y + 1.. dest_y - 1).each do |y|
+        (position_y + 1).upto(dest_y - 1) do |y|
           return true if self.game.is_occupied?(dest_x, y)
         end 
       else
-        (position_y - 1 .. dest_y + 1).each do |y|
+        (position_y - 1).downto(dest_y + 1) do |y|
           return true if self.game.is_occupied?(dest_x, y)
         end 
       end 
     elsif #Diagonol
       if self.position_x < dest_x && self.position_y < dest_y
-        (position_x + 1.. dest_x - 1).each do |x|
-          (position_y + 1.. dest_y - 1).each do |y|
+        (position_x + 1).upto(dest_x - 1) do |x|
+          (position_y + 1).upto(dest_y - 1) do |y|
             return true if self.game.is_occupied?(x, y) && (x - position_x).abs == (y - position_y).abs
           end 
         end 
       elsif self.position_x > dest_x && self.position_y < dest_y
-        (position_x - 1.. dest_x + 1).each do |x|
-          (position_y + 1.. dest_y - 1).each do |y|
+        (position_x - 1).downto(dest_x + 1) do |x|
+          (position_y + 1).upto(dest_y - 1) do |y|
             return true if self.game.is_occupied?(x, y) && (x - position_x).abs == (y - position_y).abs
           end 
         end 
       elsif self.position_x < dest_x && self.position_y > dest_y
-        (position_x + 1.. dest_x - 1).each do |x|
-          (position_y - 1.. dest_y + 1).each do |y|
+        (position_x + 1).upto(dest_x - 1) do |x|
+          (position_y - 1).downto(dest_y + 1) do |y|
             return true if self.game.is_occupied?(x, y) && (x - position_x).abs == (y - position_y).abs
           end 
         end 
       elsif self.position_x > dest_x && self.position_y > dest_y
-        (position_x - 1.. dest_x + 1).each do |x|
-          (position_y - 1.. dest_y + 1).each do |y|
+        (position_x - 1).downto(dest_x + 1) do |x|
+          (position_y - 1).downto(dest_y + 1) do |y|
             return true if self.game.is_occupied?(x, y) && (x - position_x).abs == (y - position_y).abs
           end 
         end
