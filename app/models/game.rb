a1530a8692beaf2c8f12512a9737fa6e9e0037ef
Jonamir Ian Ventura <jonianven@gmail.com>
2016-06-17 04:08:23 +0000

    Added active player field to Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ActiveRecord::Base
   belongs_to :white_player, class_name: "User"
   belongs_to :black_player, class_name: "User"
+  belongs_to :active_player, class_name: "User"
   has_many :pieces
 
 def setup
--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160610034338) do
+ActiveRecord::Schema.define(version: 20160617014432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "games", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.integer  "white_player_id"
     t.integer  "black_player_id"
     t.string   "status"
+    t.integer  "active_player_id"
   end
 
   add_index "games", ["black_player_id"], name: "index_games_on_black_player_id", using: :btree
