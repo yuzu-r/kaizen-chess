746ff3a6f723fccbc1041d2cfcf2171252544da2
Michael Guttman <mguttman@hotmail.com>
2016-06-28 04:25:14 +0000

    Revert "created conditionals in pieces controller to only allow valid player to select their own piece during their turn"
    This reverts commit b0ebfdf4b216670980925c631d3321a01c9d79f9.

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -2,24 +2,17 @@ class PiecesController < ApplicationController
   def select
     # toggles is_selected for the piece in question (select/de-select)
     # if a piece is selected, any previously selected piece is de-selected
-    
+    # note: this does NOT currently check whose turn it is!
+    # add validation to make sure the piece selected belongs to player whose turn it is 
     @piece = Piece.find(params[:id])
     @game = Game.find(params[:game_id])
-    if current_user != @piece.player 
-      #error
-    elsif 
-      @game.active_player != current_user
-      #error
-        
+    if @piece.is_selected
+      @piece.update_attributes(is_selected: false)
     else
-      if @piece.is_selected
-         @piece.update_attributes(is_selected: false)
-      else
-        @game.pieces.each do |p|
-          p.update_attributes(is_selected: false) if p.is_selected
-        end
-        @piece.update_attributes(is_selected: true)
+      @game.pieces.each do |p|
+        p.update_attributes(is_selected: false) if p.is_selected
       end
+      @piece.update_attributes(is_selected: true)
     end
     render :json => { :success => "success", :status_code => "200" }
   end
