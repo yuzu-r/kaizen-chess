e31b6f9777e8999c61f887161ca598f4f94b403b
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-07-16 03:50:58 +0000

    remove console messages

--- config/initializers/firebase.rb
+++ config/initializers/firebase.rb
@@ -1,3 +1,2 @@
-#GAMES_URI = 'https://yuzu-rtod2.firebaseio.com/games/'https://yuzu-aka.firebaseio.com/games/'
-GAMES_URI = 'https://yuzu-aka.firebaseio.com/games/'
+GAMES_URI = 'https://rtod2-caa01.firebaseio.com/games/'
 FB = Firebase::Client.new(GAMES_URI)
