080b3db22237f7d41af5ff3578368a8ab62d00e0
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-07-16 00:25:28 +0000

    clean up firebase, updateActivePlayer fix

--- config/initializers/firebase.rb
+++ config/initializers/firebase.rb
@@ -1,2 +1,3 @@
-GAMES_URI = 'https://yuzu-rtod2.firebaseio.com/games/'
+#GAMES_URI = 'https://yuzu-rtod2.firebaseio.com/games/'https://yuzu-aka.firebaseio.com/games/'
+GAMES_URI = 'https://yuzu-aka.firebaseio.com/games/'
 FB = Firebase::Client.new(GAMES_URI)
