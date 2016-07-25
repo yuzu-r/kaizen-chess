f7648f715325a5891f4049523d5f83125d01f389
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-07-25 15:39:10 +0000

    change application name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_RtoD2-chess_session'
+Rails.application.config.session_store :cookie_store, key: '_Kaizen-chess_session'
