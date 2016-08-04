7e21e1d45c9f9be14082cc996d61eefeae13f5bd
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-08-04 21:30:22 +0000

    add sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,15 @@ Rails.application.configure do
 
   # devise
   config.action_mailer.default_url_options = { host: 'kaizen-chess.herokuapp.com' }
+  
+  ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+  }  
+
 end
