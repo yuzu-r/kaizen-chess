1536bb5eaa795220fedb772b40e3fb89988a8aec
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-08-09 04:30:09 +0000

    is valid move for queen now tests for check condition

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -2,6 +2,9 @@ class Queen < Piece
 	def is_valid_move?(dest_x, dest_y)
 		return false if is_obstructed?(dest_x, dest_y) 
 		if is_diagonal_move?(dest_x, dest_y) || is_vertical_move?(dest_x, dest_y) || is_horizontal_move?(dest_x, dest_y)
+      if game.is_in_check?(player) && !resolve_check?(dest_x, dest_y)
+        return false
+      end
 			return true
 		end
 		false
--- spec/models/queen_spec.rb
+++ spec/models/queen_spec.rb
@@ -4,22 +4,39 @@ RSpec.describe Queen, type: :model do
   describe "is_valid_move? returns false if move is not allowed" do
     before :each do
       @g = FactoryGirl.create(:joined_game)
+      # note: the kings need to be defined in the game now that we check for checks
+      @white_king = King.create(position_x: 5, position_y: 1, game: @g, player: @g.white_player)
+      @black_king = King.create(position_x: 5, position_y: 8, game: @g, player: @g.black_player)
       @queen = Queen.create(position_x: 4, position_y: 4, game: @g, player: @g.white_player)
-    end
-
-    
-    it "returns true if moving 2 spaces up" do
-      expect(@queen.is_valid_move?(4,6)).to eq true
     end
     it "returns false if moves like a knight" do
     	expect(@queen.is_valid_move?(6,5)).to eq false
     end
+    it "returns false if player is in check and queen's move doesn't remove the check", :check_block => true do
+      threat = Queen.create(position_x: 5, position_y: 5, game: @g, player: @g.black_player)
+      expect(@queen.is_valid_move?(1, 4)).to eq false
+    end
+  end
+  describe "is_valid_move? returns true if move is allowed" do
+    before :each do
+      @g = FactoryGirl.create(:joined_game)
+      # note: the kings need to be defined in the game now that we check for checks
+      @white_king = King.create(position_x: 5, position_y: 1, game: @g, player: @g.white_player)
+      @black_king = King.create(position_x: 5, position_y: 8, game: @g, player: @g.black_player)
+      @queen = Queen.create(position_x: 4, position_y: 4, game: @g, player: @g.white_player)
+    end
+    it "returns true if moving 2 spaces up" do
+      expect(@queen.is_valid_move?(4,6)).to eq true
+    end
     it "returns true if moves diagonal 3 spaces" do
-    	expect(@queen.is_valid_move?(7,7)).to eq true
+      expect(@queen.is_valid_move?(7,7)).to eq true
     end 
     it "returns true if it tries to move horizontally" do
-    	expect(@queen.is_valid_move?(8,4)).to eq true
+      expect(@queen.is_valid_move?(8,4)).to eq true
     end
-   
+    it "returns true if player is in check and queen's move removes the check", :check_block => true do
+      threat = Queen.create(position_x: 5, position_y: 5, game: @g, player: @g.black_player)
+      expect(@queen.is_valid_move?(5, 4)).to eq true
+    end
   end
 end
