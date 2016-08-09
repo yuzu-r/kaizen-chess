d8b930711b98388608b81080b6c7255c1cbefe1f
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-08-09 04:21:34 +0000

    is valid move for rook now tests for check

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -2,6 +2,9 @@ class Rook < Piece
 	def is_valid_move?(dest_x, dest_y)
 		return false if is_obstructed?(dest_x, dest_y) 
   	if is_vertical_move?(dest_x, dest_y) || is_horizontal_move?(dest_x, dest_y)
+      if game.is_in_check?(player) && !resolve_check?(dest_x, dest_y)
+        return false
+      end
   		return true 
   	end  	
   	false
